04c44dfc06e02bece6f877ccea3893cbc1dfd5e5
DarthChurros <tnarayan00@stuy.edu>
2020-03-28 12:08:48 -0400

    wrote calculate_normal

--- gmath.c
+++ gmath.c
@@ -35,5 +35,20 @@ double dot_product( double *a, double *b ) {
 //Calculate the surface normal for the triangle whose first
 //point is located at index i in polygons
 double *calculate_normal(struct matrix *polygons, int i) {
-  return NULL;
+  double vec_a[3];
+  double vec_b[3];
+  int j;
+
+  for (j = 0; j < 3; j++) {
+    vec_a[j] = polygons->m[i+1][j] - polygons->m[i][j];
+    vec_b[j] = polygons->m[i+2][j] - polygons->m[i][j];
+  }
+
+  double* vec_normal = (double*)malloc(3 * sizeof(double));
+
+  vec_normal[0] = vec_a[1] * vec_b[2] - vec_a[2] * vec_b[1];
+  vec_normal[1] = vec_a[2] * vec_b[0] - vec_a[0] * vec_b[2];
+  vec_normal[2] = vec_a[0] * vec_b[1] - vec_a[1] * vec_b[0];
+
+  return vec_normal;
 }
